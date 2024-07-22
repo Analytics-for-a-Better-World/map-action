726835d9359062a15bee5d663216ed8ce321f08f
SannePipple <sanne@pipple.nl>
2024-07-22 13:52:22 +0200

    Fixed problem with gmft Improved READme

--- README.md
+++ README.md
@@ -6,15 +6,7 @@ This project aims to create a data extraction tool. It can take one or several p
 
 The code has been developed by a cooperation of ABW & Pipple.
 
-## Description of the application
-Here you can write:
-  * What the application does
-  * Why the application is created
-  * For whom the application is created
-  
-## Usage of the application
-
-### How to install the project
+## How to install the project
 Clone the project.
 
 The Python version used is 3.12.1
@@ -24,20 +16,41 @@ The requirements.txt file provides all the necessary python packages for this pr
 * Open command prompt (cmd) or a terminal in your IDE of choice and provide the following commands:
      - `pip install virtualenv`
     - cd folder/path/you/want/venv/in 
-    - `py -3.112.1 -m venv venv`
-    - cd venv/Scripts
-    - `activate`
+    - `python -m venv myenv`
+    - cd myenv/Scripts
+    - `activate.bat`
     - cd folder/path/to/requirements/file
 
 To perform the following step, it's essential that you already cloned the repo.
+
     - `pip install -r requirements.txt`
 
-### How to run the project
+The installation might take a while.
+
+## How to run the project
 Make sure to put the PDF you want tables to be extracted from in the 'input_pdf' folder of the repository.
 
-Then there are 2 scripts you can run.
+Then there are 2 scripts you can run. In the two test cases available, the first method (extract_tables_using_gmft.py), performs best. However since this does depend on the PDF as input, it might be useful to try also the second method (extract_tables_using_tabula-py.py). 
 
-#### extract_tables_using_tabula-py.py
+### Method 1: extract_tables_using_gmft.py
+This file uses [gmft](https://github.com/conjuncts/gmft) as the extraction method. 
+
+In the main function, the variable 'pdf_name' should be altered to the name of the pdf you want to be used for the extraction. Thus search for 'pdf_name' and alter the name of the PDF (where it shows now: 'Eswatini - Census 2017 - Vol 5 -EN.pdf'). The image below shows what you'll find when searching for pdf_name. Important: don't forget to add '.pdf' add the end of the name of the file, otherwise it won't recognise it.
+
+![alt text](image-1.png)
+
+After altering the name, you can run the python file with the correct environment installed.
+
+This will create a new folder in the folder output_csv, this folder will be named as the name of your pdf, followed by the current date and time of extraction. In this folder you'll see the extracted csv's which are named as the name of the table and at the end of it the word PAGE_NUMBER, followed by the page number of the document provided. This gives you the opportunity to check the pdf and its page to see whether the table is corrected properly. 
+
+### Method 2: extract_tables_using_tabula-py.py
 This file uses [tabula-py](https://pypi.org/project/tabula-py/#description) as the extraction method. 
 
-In the main function, the variable pdf_name should be altered to the name of the pdf you want to be used for the extraction.
+In the main function, the variable 'pdf_name' should be altered to the name of the pdf you want to be used for the extraction. Thus search for 'pdf_name' and alter the name of the PDF (where it shows now: 'Madagascar pag 58-73.pdf'). The image below shows what you'll find when searching for pdf_name. Important: don't forget to add '.pdf' add the end of the name of the file, otherwise it won't recognise it.
+![alt text](image.png)
+
+After altering the name, you can run the python file with the correct environment installed.
+
+This will create a new folder in the folder output_csv, this folder will be named as the name of your pdf, followed by the current date and time of extraction. In this folder you'll see the extracted csv's which are named as the name of the table and at the end of it the word PAGE_NUMBER, followed by the page number of the document provided. This gives you the opportunity to check the pdf and its page to see whether the table is corrected properly. 
+
+When using this method, you might find that some of the pdf's are named 'CHANGED_name_of_pdf'. These are the csv's that, after being extracted, are ran through the cleaning functions, to obtain a better format of the tables. In the same folder you'll also find the original extracted csv (thus with the same name but without CHANGED_ in front of it), which you could also check to see if it has indeed improved.
--- extract_tables_using_gmft.py
+++ extract_tables_using_gmft.py
@@ -1,4 +1,4 @@
-##### THIS FILE EXTRACTS AND CLEANS TABLES FROM A PDF FILE USING the gmft package.#####
+##### THIS FILE EXTRACTS AND CLEANS TABLES FROM A PDF FILE USING THE gmft PACKAGE.#####
 
 # NOTES regarding the code:
 # 1. The gmft package has a function which can extract the caption of a table. However this function does not work yet for 
@@ -6,13 +6,13 @@
 # Thus it might be something to keep in mind for future use.
 
 # 2. A strange problem was encountered when trying to save the tables in a different function then where they have been extracted.
-# Thus it was chosen to save the tables in the same function as where they were extracted them, even though this does not help the readability of the code.
+# Thus it was chosen to save the tables in the same function as where they were extracted (extract_tables_captions_and_save), 
+# even though this does not help the readability of the code.
 # -------------------------------------------------------------- 
 import fitz  # PyMuPDF
 import os
 import pandas as pd
 import re
-import tabula
 from datetime import datetime
 from gmft.pdf_bindings import PyPDFium2Document
 from gmft import CroppedTable, TableDetector
@@ -20,7 +20,6 @@ from gmft import AutoTableFormatter
 from gmft import AutoFormatConfig
 from gmft import TATRFormatConfig
 from IPython.display import display
-from tabula.io import read_pdf
 from unidecode import unidecode
 
 # Set the configuration for the table extraction
@@ -28,7 +27,10 @@ config = AutoFormatConfig()
 config.enable_multi_header = True
 config.semantic_spanning_cells = True
 TATRFormatConfig.force_large_table_assumption = False
+TATRFormatConfig.total_overlap_reject_threshold = 0.3
 config.large_table_assumption = False
+
+detector = TableDetector()
 
 formatter = AutoTableFormatter()
 
@@ -53,16 +55,24 @@ def obtain_pdf_path(pdf_name: str) -> str:
     return pdf_path
 
 def ingest_pdf(pdf_path) -> list[CroppedTable]:
-    doc = PyPDFium2Document(pdf_path)
+    """
+    Ingests a PDF document and extracts tables from it.
 
-    detector = TableDetector()
+    Args:
+        pdf_path (str): The file path of the PDF document to extract tables from.
+
+    Returns:
+        A list of CroppedTable objects representing the tables extracted from the PDF document.
+    """
+
+    doc = PyPDFium2Document(pdf_path)
 
     tables = []
     for page in doc:
         tables += detector.extract(page)
-    return tables
+    return tables, doc
 
-def extract_tables_and_captions(pdf_path, pdf_name, confidence_threshold = 0.995) -> list:
+def extract_tables_captions_and_save(pdf_path, pdf_name, confidence_threshold = 0.995) -> list:
     """
     Extracts tables and their corresponding captions from a PDF document.
 
@@ -73,7 +83,7 @@ def extract_tables_and_captions(pdf_path, pdf_name, confidence_threshold = 0.995
         A list of tuples, where each tuple contains a table caption (str) and a pandas DataFrame object representing the
         table data.
     """
-    tables = ingest_pdf(pdf_path)
+    tables, doc = ingest_pdf(pdf_path)
 
     pdf_document = fitz.open(pdf_path)
     tables_and_captions = []
@@ -113,11 +123,12 @@ def extract_tables_and_captions(pdf_path, pdf_name, confidence_threshold = 0.995
                 if candidate_caption.startswith(("table", "tableau", "table", "quadro", "tabela")) \
                     and 'below shows' not in candidate_caption\
                     and 'above shows' not in candidate_caption\
+                    and 'shows' not in candidate_caption\
                     and 'table des matieres' not in candidate_caption\
                     and 'indice' not in candidate_caption\
                     and 'índice' not in candidate_caption\
                     and re.search('\d', candidate_caption):
-                    tables_and_captions.append((candidate_caption_original, table))
+                    tables_and_captions.append((candidate_caption_original, table, page_number))
                     break
 
     # Close the PDF document 
@@ -130,41 +141,38 @@ def extract_tables_and_captions(pdf_path, pdf_name, confidence_threshold = 0.995
 
     os.makedirs(folder_path, exist_ok=True)
 
-    tables_and_captions_above_threshold = [(caption, table) for caption, table in tables_and_captions if table.confidence_score >= confidence_threshold]
+    tables_and_captions_above_threshold = [(caption, table, page_number) for caption, table, page_number in tables_and_captions if table.confidence_score >= confidence_threshold]  
 
     # Save each table as a CSV file
-    for caption, table in tables_and_captions_above_threshold:
+    for caption, table, page_number in tables_and_captions_above_threshold:
         # Remove any invalid characters from the filename
         sanitized_filename = "".join([c if c.isalnum() else "_" for c in caption])
-        sanitized_filename = f"{sanitized_filename}.csv"
-        
+        sanitized_filename = f"{sanitized_filename}_PAGE_NUMBER_{page_number+1}.csv"
+
         # Extract the table from the formatted text using the extractor function
         ft = formatter.extract(table)
-        try:
-            # Set an option to display the DataFrame without multi-level indexing
-            with pd.option_context('display.multi_sparse', False):
-                # Convert the formatted text to a pandas DataFrame
-                df_table = ft.df()
-                # Replace any NaN values in the DataFrame with an empty string
-                df_table.fillna("", inplace=True)
+        # Set an option to display the DataFrame without multi-level indexing
+        with pd.option_context('display.multi_sparse', False):
+            # Convert the formatted text to a pandas DataFrame
+            df_table = ft.df()
+            # Replace any NaN values in the DataFrame with an empty string
+            df_table.fillna("", inplace=True)
 
-            # Save the table as a CSV file in the folder, choose encoding="ANSI" for French characters
-            df_table.to_csv(os.path.join(folder_path, sanitized_filename), index=False, sep=";", encoding="utf-8")
-        except Exception as e:
-            print(f"Error saving table '{caption}': {e}")
+        # Save the table as a CSV file in the folder, choose encoding="ANSI" for French characters
+        df_table.to_csv(os.path.join(folder_path, sanitized_filename), index=False, sep=";", encoding="utf-8")
 
     return tables_and_captions
 
 # Main function
 def main():
     # Define the name of the PDF you want to extract tables from
-    pdf_name = 'Madagascar pag 58-73.pdf'
+    pdf_name = 'Eswatini - Census 2017 - Vol 5 - EN.pdf'
 
     # Obtain the path of the PDF file to extract tables from
     pdf_path = obtain_pdf_path(pdf_name)
-
+    
     # Extract the tables and captions from the PDF document
-    tables_and_captions = extract_tables_and_captions(pdf_path, pdf_name)
+    tables_and_captions = extract_tables_captions_and_save(pdf_path, pdf_name)
     
     # # In case tables and captions are found, save them as CSV files and clean them
     if not tables_and_captions:
